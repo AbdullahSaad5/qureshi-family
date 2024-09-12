db84f44d277895f4f5f28e98793db61c96b02c32
sami2022skipq <abdul.sami.skipq@gmail.com>
2024-09-12 18:54:43 +0500

    Minor changes

--- src/app/Admin/components/Header/DropdownUser.tsx
+++ src/app/Admin/components/Header/DropdownUser.tsx
@@ -31,7 +31,7 @@ const DropdownUser = ({}) => {
       >
         <span className="hidden text-right lg:block">
           <span className="block text-sm font-medium text-black dark:text-white">
-            {user?.fullName || "Thomas"}
+            {user?.fullName || "-"}
           </span>
           <span className="block text-xs">{user?.designation || "admin"}</span>
         </span>
@@ -72,7 +72,7 @@ const DropdownUser = ({}) => {
           className={`absolute right-0 mt-4 flex w-62.5 flex-col rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark`}
         >
           <ul className="flex flex-col gap-5 border-b border-stroke px-6 py-7.5 dark:border-strokedark">
-            <li>
+            {/* <li>
               <Link
                 href="/Admin/profile"
                 className="flex items-center gap-3.5 text-sm font-medium duration-300 ease-in-out hover:text-primary lg:text-base"
@@ -96,7 +96,7 @@ const DropdownUser = ({}) => {
                 </svg>
                 My Profile
               </Link>
-            </li>
+            </li> */}
             <li>
               <Link
                 href="/Admin/settings"
